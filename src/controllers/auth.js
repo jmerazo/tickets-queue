79a90870483e9b42cdbc45c02c274afc2667ea2e
jmerazo <jmerazo96@gmail.com>
2022-07-25 20:23:05 -0500

    Auth controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,6 +1,25 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const validateEmailController = require('../controllers/users')
 
-const loginCreate = async () => {
+const loginCreate = async (req, res, next) => {
+    try {
+        const {username, password} = req.body;
+        
+        if(!username || !password){
+            res.status(400).send('All input is required');
+        }
+
+        const validateEmail = await validateEmailController.getUserByEmailController(username);
+
+        if(validateEmail){
+            return res.status(409).send('User already exist. Please login');
+        }
+
+        encryptedPassword = await bcr
+    } catch (error) {
+        
+    }
 
 }
 
